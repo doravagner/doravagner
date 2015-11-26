d83da12f913540dc6af95588d9d5b992c003214c
Stefan Veis Pennerup <stefanveispennerup@gmail.com>
2015-11-26 09:19:24 +0100

    Added an empty css and javascript file

---
+++ js/index.js
@@ -0,0 +1,20 @@
+/*******************************************************************************
+    index.js
+    Dóra Vagner's Portfolio
+
+    Created by Dóra Vagner in November, 2015.
+    Copyright Dóra Vagner 2015. All rights reserved.
+
+*******************************************************************************/
+
+$(document).ready(function () {
+
+  // VIEWS----------------------------------------------------------------------
+  $helloButton = $("#hello");
+
+  // CALLBACKS------------------------------------------------------------------
+  $helloButton.click(function () {
+    console.log("You clicked me!!!");
+  });
+
+});
